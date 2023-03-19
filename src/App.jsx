4e19b7e751d6cbfd2000683b4c5ec5c66636e5ef
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-19 13:01:52 -0400

    configuation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1 class="w-64 h-64 bg-red-900">Hello world!</h1>;
+}
+
+export default App;

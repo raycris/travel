688551a4bb12e74717fb3b8cfb586954aed708f0
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-20 14:53:42 -0400

    some skeletors HTML

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = ({id}) => {
+  return (
+    <div id={id}>Home</div>
+  )
+}
+
+export default Home
---
+++ src/components/Recommended.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recommended = ({id}) => {
+  return (
+    <div id={id}>Recommended</div>
+  )
+}
+
+export default Recommended

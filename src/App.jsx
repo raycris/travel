c054a52a79d48c9a3024f74d5fa02b02182c1cb2
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-20 15:37:56 -0400

    home component done

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,24 @@ import { useState } from "react";
 
 import Home from "./components/Home";
 import Recommended from "./components/Recommended";
-import PopularRents from "./components/PopularRents";
-import Footer from "./components/Footer";
+import PopularRents from "./components/PopularRenst";
+// import Footer from "./components/Footer";
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
+ 
 
   return (
     <div>
       <nav></nav>
       <div id="tab_bar"></div>
-      <section>
+      <section className="w-full h-screen">
         <Home id="home" />
-        <Recommended id="recommended" />
-        <PopularRents id="pupular_rents" />
+        {/* <Recommended id="recommended" />
+        <PopularRents id="pupular_rents" /> */}
       </section>
-      <Footer />
-      {/* <h1 className="font-Montserrat text-xl">Hello world!</h1>
-      <div className="w-8 h-8 bg-purple">padre tiempo</div> */}
+      {/* <Footer /> */}
     </div>
   );
 }

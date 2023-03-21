a4ff58aa7aa7955e495bd6aca77e853a143aa61e
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-21 17:08:57 -0400

    transition and transform in button and input

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,14 @@ const Home = ({ id }) => {
     <div id={id}>
       <div className="w-full h-3/4">
         <div className="w-full h-full flex flex-col absolute space-y-80 py-4 items-center ">
-          <input className="outline-none p-3 rounded-xl shadow-sm" type="search" placeholder="San Francisco"/>
-          <button className="text-xl text-primary font-semibold p-4 w-44 rounded-md bg-white">Explore more</button>
+          <input
+            className="outline-none p-3 rounded-xl shadow-sm transition duration-300 focus-within:shadow-sm focus:ring-2 focus:w-11/12"
+            type="search"
+            placeholder="San Francisco"
+          />
+          <button className="text-xl text-primary font-semibold p-4 w-44 rounded-md bg-white transition-all duration-500 ease-in-out hover:bg-primary hover:text-white transform hover:-translate-y-1 hover:scale-110">
+            Explore more
+          </button>
         </div>
         <div className="w-full h-full">
           <img src={SanFran} alt="San Francisco" />

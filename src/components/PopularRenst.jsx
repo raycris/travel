329caa8edce71eae76fe5a011c4b5fb268856639
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-22 16:16:36 -0400

    faq component section done

--- src/components/PopularRenst.jsx
+++ src/components/PopularRenst.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 
-import Chicago from "../assets/images/chicago.jpg"
+import Chicago from "../assets/images/chicago.jpg";
+
+import FAQ from "./FAQ";
 
 const PopularRenst = ({ id }) => {
   return (
-    <div className="px-2 h-auto w-full" id={id}>
-      <p className="text-3xl text-primary font-semibold pb-6 mt-6">Trending Stays</p>
-      <div className="w-full h-auto flex flex-col space-y-6 items-center justify-center">
-        <div className="w-full h-96 bg-tertiary  bg-cover rounded-lg">
-          <picture>
-            <img className=" rounded-lg" src={Chicago} alt="Chicago"  />
-          </picture>
-          <p className="CardTitle bg-tertiary">Chicago</p>
-          <p className="text-sm pl-8 text-black mr-24">2 rooms, 1 bathroom, 1 kitchen</p>
-        </div>
-        <div className="w-full h-96 bg-tertiary  bg-cover rounded-lg">
-          <picture>
-            <img className="rounded-xl" src={Chicago} alt="Chicago"  />
-          </picture>
-          <p className="CardTitle">Chicago</p>
-          <p className="text-sm pl-8 text-black mr-24">2 rooms, 1 bathroom, 1 kitchen</p>
+    <>
+      <div className="px-2 h-auto w-full" id={id}>
+        <p className="text-3xl text-primary font-semibold pb-6 mt-6">
+          Trending Stays
+        </p>
+        <div className="w-full h-auto flex flex-col space-y-6 items-center justify-center">
+          <div className="w-full h-96 bg-primary  bg-cover rounded-lg">
+            <picture>
+              <img className=" rounded-t" src={Chicago} alt="Chicago" />
+            </picture>
+            <p className="CardTitle ">Chicago</p>
+            <p className="text-sm pl-8 text-black mr-24">
+              2 rooms, 1 bathroom, 1 kitchen
+            </p>
+          </div>
+          <div className="w-full h-96 bg-primary  bg-cover rounded-lg">
+            <picture>
+              <img className="rounded-t" src={Chicago} alt="Chicago" />
+            </picture>
+            <p className="CardTitle">Chicago</p>
+            <p className="text-sm pl-8 text-black mr-24">
+              2 rooms, 1 bathroom, 1 kitchen
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <FAQ id="faqs" />
+    </>
   );
 };
 

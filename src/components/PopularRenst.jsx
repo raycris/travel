ec97819b8e507caeed99364c9a63bccc4742f432
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-23 15:51:49 -0400

    grind with trending-stays done

--- src/components/PopularRenst.jsx
+++ src/components/PopularRenst.jsx
@@ -5,70 +5,68 @@ import Miami from "../assets/images/miami.jpg";
 import Bali from "../assets/images/bali.jpg";
 import LA from "../assets/images/LA.jpg";
 
-import FAQ from "./FAQ";
 
 const PopularRenst = ({ id }) => {
   return (
-    <section >
-      <div className="px-6 h-full lg:px-14" id={id}>
-        <p className="text-3xl font-semibold text-primary pb-6">
-          Trending Stays
-        </p>
-        <div className="w-full h-full flex flex-col items-center justify-center lg:flex-col">
-          <div className="w-full h-96 bg-chicago bg-cover rounded-2xl mb-8 lg:flex-none lg:bg-center">
+    <section>
+      <div id={id} class="p-6 h-full w-full ">
+        <p class="text-3xl text-primary font-semibold">Trending Stays</p>
+        <div class="w-full h-full flex flex-col space-y-4 items-center justify-center mt-6 ">
+          <div class="w-full h-96 bg-chicago bg-cover rounded-xl lg:h-[38rem] lg:text-right lg:bg-center ">
             <picture>
               <img
-                className=" w-full h-96 bg-chicago bg-cover rounded-2xl mb-8 lg:flex-none lg:bg-center "
+                className="w-full h-96 bg-chicago bg-cover rounded-xl lg:h-[38rem] lg:text-right lg:bg-center "
                 src={Chicago}
-                alt="Chicago"
+                alt="Miami"
               />
             </picture>
-            <p className="CardTitle lg:text-black">Chicago</p>
-            <p className="text-md pl-8 text-white mr-24 lg:text-black">
-              2 rooms, 1 bathroom, 1 kitchen
-            </p>
+            <p class="CardTitle">Chicago</p>
+            {/* <p class="text-sm pl-8 text-black mr-24 lg:mr-8 lg:text-base">
+              2 rooms, bathroom and kitchen.
+            </p> */}
           </div>
-          <div className="lg:flex lg:w-full lg:h-1/3 lg:flex-none">
-            <div className="w-full h-96 bg-LA bg-cover rounded-2xl mb-8 lg:w-3/5 lg:h-auto">
+
+          <div class="w-full h-full grid grid-cols-1 lg:grid-cols-5 lg:grid-rows-2 lg:place-items-center lg:place-content-stretch gap-4">
+            <div class="w-full h-96 bg-LA bg-cover rounded-xl lg:row-span-2 lg:col-span-2 lg:h-[49rem] lg:text-right lg:bg-center ">
               <picture>
                 <img
-                  className="w-full h-96 bg-LA bg-cover rounded-2xl mb-8 lg:w-auto lg:h-auto"
+                  className="w-full h-96 bg-LA bg-cover rounded-xl lg:row-span-2 lg:col-span-2 lg:h-[49rem] lg:text-right lg:bg-center  "
                   src={LA}
-                  alt="Los Angeles"
+                  alt="LA"
                 />
               </picture>
-              <p className="CardTitle">Los Angeles</p>
-              <p className="text-md pl-8 text-white mr-24">
-                2 rooms, 1 bathroom, 1 kitchen
-              </p>
+              <p class="CardTitle">Los Angeles</p>
+              {/* <p class="text-sm pl-8 text-black mr-24 lg:mr-8 lg:text-base">
+                4 rooms, 3 bathrooms, kitchen and private pool.
+              </p> */}
             </div>
-            <div className="lg:flex lg:flex-col lg:w-full lg:pl-8 ">
-              <div className="w-full h-96 bg-miami bg-cover rounded-2xl mb-8 ">
-                <picture>
-                  <img
-                    className="w-full h-96 bg-miami bg-cover rounded-2xl mb-8 "
-                    src={Miami}
-                    alt="Miami"
-                  />
-                </picture>
-                <p className="CardTitle">Miami</p>
-                <p className="text-md pl-8 text-white mr-24">
-                  2 rooms, 1 bathroom, 1 kitchen
-                </p>
-              </div>
-              <div className="w-full h-96 bg-bali bg-cover rounded-2xl mb-8 ">
-                <picture>
-                  <img
-                    className="w-full h-96 bg-bali bg-cover rounded-2xl mb-8 "
-                    src={Bali}
-                    alt="Bali"
-                  />
-                </picture>
-                <p className="CardTitle">Bali</p>
-                <p className="text-md pl-8 text-white mr-24 ">
-                  2 rooms, 1 bathroom, 1 kitchen
-                </p>
-              </div>
+
+            <div class="w-full h-96 bg-miami bg-cover rounded-xl lg:row-span-1 lg:col-span-3 lg:text-right lg:bg-center ">
+              <picture>
+                <img
+                  className="w-full h-96 bg-miami bg-cover rounded-2xl mb-8 "
+                  src={Bali}
+                  alt="Bali"
+                />
+              </picture>
+              <p class="CardTitle">Miami</p>
+              {/* <p class="text-sm pl-8 text-black mr-24 lg:mr-8 lg:text-base">
+                3 rooms, 2 bathrooms, kitchen and amazing sea view.
+              </p> */}
+            </div>
+
+            <div class="w-full h-96 bg-bali bg-cover rounded-xl lg:row-span-1 lg:col-span-3 lg:text-right lg:bg-center ">
+              <picture>
+                <img
+                  className="w-full h-96 bg-miami bg-cover rounded-2xl mb-8 "
+                  src={Miami}
+                  alt="Miami"
+                />
+              </picture>
+              <p class="CardTitle">Bali</p>
+              {/* <p class="text-sm pl-8 text-black mr-24 lg:mr-8 lg:text-base">
+                2 rooms, 2 bathrooms, kitchen and private pool.
+              </p> */}
             </div>
           </div>
         </div>
